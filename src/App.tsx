6ee8cd0042497407642590927f18194621177c9e
SuriyaSR <suriyasun19@gmail.com>
2025-05-28 23:06:13 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+//https://www.youtube.com/watch?v=kRCcQ-h4buM
+
+
+import './App.css'
+
+function App() {
+
+  return (    
+    <div>        
+      <h1 className="text-center">React Quiz App</h1>      
+    </div>
+  )
+}
+
+export default App
